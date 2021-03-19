aadadb51f5936833031130a03aa89e69d3dbc6d2
alfredsadjei <alfredadjei2012@gamil.com>
2021-03-19 21:40:54 +0000

    initial commit

---
+++ src/test/java/com/TradeEngine/tradeSystem/TradeSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TradeEngine.tradeSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
