4a1bf3c1ea650fe1e4defe1e846ecad37f5a280f
alfredsadjei <alfredadjei2012@gamil.com>
2021-03-21 20:27:05 +0000

    cahnged startup method for tradeEngine to use event listener

--- src/main/java/com/TradeEngine/tradeSystem/services/TradeEngineService.java
+++ src/main/java/com/TradeEngine/tradeSystem/services/TradeEngineService.java
@@ -5,21 +5,22 @@ import com.TradeEngine.tradeSystem.TradeEngineRedisClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 
 
 @Component
-public class TradeEngineService implements ApplicationRunner {
+public class TradeEngineService{
 
     @Autowired
     TradeEngineRedisClient tradeEngineRedisClient;
 
     TradeEnginePubSub tradeEnginePubSub = new TradeEnginePubSub();
 
-    @Override
-    public void run(ApplicationArguments args) throws Exception {
-
+    @EventListener(ApplicationReadyEvent.class)
+    public void run(){
         //have redisClient connect pubsub(subscriber) and channel
         tradeEngineRedisClient.connect().subscribe(tradeEnginePubSub,"orderCreated");
     }
