588756fbdfc2d3e0ef0841099cb70fe1892c6430
alfredsadjei <alfredadjei2012@gamil.com>
2021-03-27 13:33:08 +0000

    specified subscriber to have no timeout

--- src/main/java/com/TradeEngine/tradeSystem/utils/TradeEngineRedisClient.java
+++ src/main/java/com/TradeEngine/tradeSystem/utils/TradeEngineRedisClient.java
@@ -1,10 +1,12 @@
 package com.TradeEngine.tradeSystem.utils;
 
-import com.TradeEngine.tradeSystem.utils.RedisServer;
+
 import redis.clients.jedis.Jedis;
 
 final public class TradeEngineRedisClient {
-    private static final Jedis jedis = new Jedis("redis-17849.c59.eu-west-1-2.ec2.cloud.redislabs.com",17849);
+
+    //Specify no timeout with the last parameter
+    private static final Jedis jedis = new Jedis("redis-17849.c59.eu-west-1-2.ec2.cloud.redislabs.com",17849,0);
 
     public static Jedis connect() {
         jedis.auth(RedisServer.SERVER_KEY.getKeyVal());
